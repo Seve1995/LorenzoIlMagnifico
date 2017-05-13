630d073adbb25cf7fa4d22a9b36d8740acbea154
Seve1995 <giuseppe.severino@mail.polimi.it>
2017-05-13 12:51:26 +0200

    Renamed "CounsilPalaceCell" with "CouncilPalaceCell"

--- src/main/java/it/polimi/ingsw/pc22/gamebox/CouncilPalaceCell.java
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/CouncilPalaceCell.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.pc22.gamebox;
 
 import it.polimi.ingsw.pc22.player.Player;
 
-public class CounsilPalaceCell extends Cell {
+public class CouncilPalaceCell extends Cell {
 
 	@Override
 	public void executeEffect(Player player) {
