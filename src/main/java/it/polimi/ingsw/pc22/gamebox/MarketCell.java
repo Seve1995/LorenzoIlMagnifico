a4764d252fc67e50cddd923e40029ae894b03230
Matteo Petrini <matteo.petrini@live.com>
2017-05-13 12:45:13 +0200

    Added Cells: Harvest, Market, Prod.

---
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/MarketCell.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.pc22.gamebox;
+
+import it.polimi.ingsw.pc22.player.Player;
+
+public class MarketCell extends Cell {
+	private boolean blockedCell;
+	
+	@Override
+	public void executeEffect(Player player) {
+		// TODO Auto-generated method stub	
+	}
+	
+
+}
