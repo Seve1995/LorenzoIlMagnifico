1bbcc35b943cd04e3d2f1264416bf2cdb2e43390
Matteo Petrini <matteo.petrini@live.com>
2017-05-15 19:20:19 +0200

    Added Council Privileges

---
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/CouncilPrivilage.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.pc22.gamebox;
+
+import it.polimi.ingsw.pc22.effects.Effect;
+
+public class CouncilPrivilage extends Effect {
+	
+	private void choosePrivileges(){};
+	private void chooseTwoDifferentPrivileges(){};
+	private void chooseThreeDifferentPrivileges(){};
+
+}
