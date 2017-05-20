5c77d501f21021c31d8349891bacad3e1c6d0d8f
Seve1995 <giuseppe.severino@mail.polimi.it>
2017-05-20 09:57:41 +0200

    Renamed class "CouncilPrivilege" to the correct name. Removed its duplicated.

--- src/main/java/it/polimi/ingsw/pc22/gamebox/CouncilPrivilege.java
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/CouncilPrivilege.java
@@ -1,6 +1,22 @@
 package it.polimi.ingsw.pc22.gamebox;
 
-public class CouncilPrivilege  extends Asset
-{
+import it.polimi.ingsw.pc22.effects.Effect;
+import it.polimi.ingsw.pc22.player.Player;
+
+public class CouncilPrivilage extends Effect {
 	
+	@Override
+	public boolean isLegal(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void executeAction(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+	private void choosePrivileges(){};
+	private void chooseTwoDifferentPrivileges(){};
+	private void chooseThreeDifferentPrivileges(){};
+
 }
