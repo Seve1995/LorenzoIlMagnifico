9f6fc14c27145bd0417fe27956d57ccb148e2b53
Matteo Petrini <matteo.petrini@live.com>
2017-05-23 11:09:35 +0200

    fixed a comment

--- src/main/java/it/polimi/ingsw/pc22/effects/RemoveAssetsFromTower.java
+++ src/main/java/it/polimi/ingsw/pc22/effects/RemoveAssetsFromTower.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.pc22.player.Player;
  * EFFECT DESCRIPTION:
  * You don't take the bonuses when you take a Development Card from the third
  * and the fourth floor or  the towers (through a Family Member or as an 
- * effect of another card.
+ * effect of another card).
  */
 
 public class RemoveAssetsFromTower extends Effect{
