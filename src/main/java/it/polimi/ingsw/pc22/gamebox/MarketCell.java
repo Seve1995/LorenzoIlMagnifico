5b70cd5a168c3f714dede9741da084dacd20beec
Seve <Seve@10.169.218.116>
2017-05-23 13:06:01 +0200

    Converted MarketCell in Market to ArrayList instead of a normal Array. Fixed errors in SettingFamiliarMemberOnMarket

--- src/main/java/it/polimi/ingsw/pc22/gamebox/MarketCell.java
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/MarketCell.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.pc22.gamebox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.polimi.ingsw.pc22.effects.Effect;
+import it.polimi.ingsw.pc22.effects.GainAsset;
 import it.polimi.ingsw.pc22.player.Player;
 
 public class MarketCell extends Cell {
@@ -44,15 +46,14 @@ public class MarketCell extends Cell {
 	
 	/*
 	 * EXAMPLE:
-	 * 
-	  	public static void main(String[] args){
-		Asset asset = new Asset(3, AssetType.COIN);
-		Asset asset2 = new Asset(3, AssetType.FAITHPOINT);
-		Effect[] effect = new Effect[2];
-		effect[0] = new GainAsset(null, asset);
-		effect[1] = new GainAsset(null, asset2);
-		MarketCell m1 = new MarketCell(3, effect);
-		System.out.print(m1.toString());
+		public static void main(String[] args){
+			Asset asset = new Asset(3, AssetType.COIN);
+			Asset asset2 = new Asset(3, AssetType.FAITHPOINT);
+			List<Effect> effects = new ArrayList<Effect>();
+			effects.add(new GainAsset(asset));
+			effects.add(new GainAsset(asset2));
+			MarketCell m1 = new MarketCell(3, effects);
+			System.out.print(m1.toString());
 	}
 	*/
 }
