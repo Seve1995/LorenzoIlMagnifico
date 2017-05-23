6a720b73dfad0562d8fe26d3edf0581abc0b9823
Matteo Petrini <matteo.petrini@live.com>
2017-05-23 14:16:28 +0200

    toStringTower

--- src/main/java/it/polimi/ingsw/pc22/gamebox/Tower.java
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/Tower.java
@@ -1,13 +1,16 @@
 package it.polimi.ingsw.pc22.gamebox;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Tower {
 	private CardTypeEnum towerType;
-	private TowerCell[] towerCells;
+	private List<TowerCell> towerCells;
 	
 	public Tower(CardTypeEnum towerType)
 	{
 		this.towerType = towerType;
-		towerCells = new TowerCell[4];
+		towerCells = new ArrayList<TowerCell>();
 	}
 	
 	public CardTypeEnum getTowerType() {
@@ -18,13 +21,22 @@ public class Tower {
 		this.towerType = towerType;
 	}
 
-	public TowerCell[] getTowerCells() {
+	public List<TowerCell> getTowerCells() {
 		return towerCells;
 	}
 
-	public void setTowerCells(TowerCell[] towerCells) {
+	public void setTowerCells(List<TowerCell> towerCells) {
 		this.towerCells = towerCells;
 	}
 	
+	@Override
+	public String toString(){
+		String output=this.towerType.toString()+"/n";
+		for(TowerCell t : towerCells)
+		{
+			output = t.toString()+"/n";
+		}
+		return output;
+	}
 	
 }
