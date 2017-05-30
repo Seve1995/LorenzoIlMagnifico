5bc39d90b0ae512519ee0e78444d288527d25ba1
Matteo Petrini <matteo.petrini@live.com>
2017-05-30 18:33:52 +0200

    Implementation of Leader Cards

--- src/main/java/it/polimi/ingsw/pc22/actions/DiscardLeaderCard.java
+++ src/main/java/it/polimi/ingsw/pc22/actions/DiscardLeaderCard.java
@@ -6,7 +6,7 @@ import it.polimi.ingsw.pc22.player.Player;
 
 public class DiscardLeaderCard extends Action{
 	
-	private LeaderCard leaderCard;
+	private int index;
 
 	@Override
 	protected boolean isLegal(Player player) {
@@ -17,8 +17,8 @@ public class DiscardLeaderCard extends Action{
 	@Override
 	public boolean executeAction(Player player) {
 		
-		int i=1;//valore fittizio
-		player.getLeaderCards().remove(i);
+		
+		player.getLeaderCards().remove(index);
 		PickOneCouncilPrivilege currCouncilPrivilege=new PickOneCouncilPrivilege();
 		currCouncilPrivilege.executeEffect(player);
 		return true;
