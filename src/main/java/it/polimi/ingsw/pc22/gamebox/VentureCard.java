5f036ddf40455f0f54403fa0e624099c1589885c
Seve1995 <giuseppe.severino@mail.polimi.it>
2017-05-31 10:06:26 +0200

    Reimplemented VentureCard

--- src/main/java/it/polimi/ingsw/pc22/gamebox/VentureCard.java
+++ src/main/java/it/polimi/ingsw/pc22/gamebox/VentureCard.java
@@ -7,37 +7,118 @@ import java.util.List;
 
 public class VentureCard extends DevelopmentCard
 {
-	private List<Asset> resourceCost; 
-	private Asset militaryCost;
-	private int militaryPointsGain;
+	private Asset militaryPointsRequired;
+	private Asset militaryPointsCost;
+	private List<Asset> resourcesCost; 
 	private boolean requiredCostChoice = false;
 	
 	
-	public VentureCard(String name, int roundNumber, List<Effect> immediateEffects, List<Effect> permanentEffects, List<Asset> resourceCost, Asset militaryCost, int militaryPointsGain) 
+	public VentureCard
+	(
+			String name, int roundNumber, List<Effect> immediateEffects, List<Effect> permanentEffects, 
+			Asset militaryPointsRequired, Asset militaryPointsCost, List<Asset> resourcesCost, boolean requiredCostChoice
+	) 
 	{
 		super(name, roundNumber, immediateEffects, permanentEffects);
-		this.resourceCost = resourceCost;
-		this.militaryCost = militaryCost;
+		this.militaryPointsRequired = militaryPointsRequired;
+		this.militaryPointsCost = militaryPointsCost;
+		this.resourcesCost = resourcesCost;
+		this.requiredCostChoice = requiredCostChoice;
 	}
 	
-	public List<Asset> getResourceCost() {
-		return resourceCost;
+	
+
+	public Asset getMilitaryPointsRequired() {
+		return militaryPointsRequired;
 	}
-	public void setResourceCost(List<Asset> resourceCost) {
-		this.resourceCost = resourceCost;
+
+
+
+	public void setMilitaryPointsRequired(Asset militaryPointsRequired) {
+		this.militaryPointsRequired = militaryPointsRequired;
 	}
-	public Asset getMilitaryCost() {
-		return militaryCost;
+
+
+
+	public Asset getMilitaryPointsCost() {
+		return militaryPointsCost;
 	}
-	public void setMilitaryCost(Asset militaryCost) {
-		this.militaryCost = militaryCost;
+
+
+
+	public void setMilitaryPointsCost(Asset militaryPointsCost) {
+		this.militaryPointsCost = militaryPointsCost;
 	}
-	public int getMilitaryPointsGain() {
-		return militaryPointsGain;
+
+
+
+	public List<Asset> getResourcesCost() {
+		return resourcesCost;
 	}
-	public void setMilitaryPointsGain(int militaryPointsGain) {
-		this.militaryPointsGain = militaryPointsGain;
+
+
+
+	public void setResourcesCost(List<Asset> resourcesCost) {
+		this.resourcesCost = resourcesCost;
 	}
+
+
+
+	public boolean isRequiredCostChoice() {
+		return requiredCostChoice;
+	}
+
+
+
+	public void setRequiredCostChoice(boolean requiredCostChoice) {
+		this.requiredCostChoice = requiredCostChoice;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((militaryPointsCost == null) ? 0 : militaryPointsCost.hashCode());
+		result = prime * result + ((militaryPointsRequired == null) ? 0 : militaryPointsRequired.hashCode());
+		result = prime * result + (requiredCostChoice ? 1231 : 1237);
+		result = prime * result + ((resourcesCost == null) ? 0 : resourcesCost.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof VentureCard))
+			return false;
+		VentureCard other = (VentureCard) obj;
+		if (militaryPointsCost == null) {
+			if (other.militaryPointsCost != null)
+				return false;
+		} else if (!militaryPointsCost.equals(other.militaryPointsCost))
+			return false;
+		if (militaryPointsRequired == null) {
+			if (other.militaryPointsRequired != null)
+				return false;
+		} else if (!militaryPointsRequired.equals(other.militaryPointsRequired))
+			return false;
+		if (requiredCostChoice != other.requiredCostChoice)
+			return false;
+		if (resourcesCost == null) {
+			if (other.resourcesCost != null)
+				return false;
+		} else if (!resourcesCost.equals(other.resourcesCost))
+			return false;
+		return true;
+	}
+
+
 
 	@Override
 	public void useImmediateEffect(Player player) {
@@ -49,14 +130,16 @@ public class VentureCard extends DevelopmentCard
 		// TODO Auto-generated method stub
 		
 	}
+	
+	//TODO: Rifare con la nuova logica implementata
 	@Override
 	public String toString() {
 		
 		String output = this.getName() + " is a Venture Card.\n Its activation cost are ";
 		
-		for (Asset a: this.resourceCost){ 
+		for (Asset a: this.resourcesCost){ 
 			 output += a.toString() + "\n";
-			 if (this.requiredCostChoice && !(a.equals(resourceCost.get(resourceCost.size()-1))))
+			 if (this.requiredCostChoice && !(a.equals(resourcesCost.get(resourcesCost.size()-1))))
 			 {
 				 output += "or";
 			 }
