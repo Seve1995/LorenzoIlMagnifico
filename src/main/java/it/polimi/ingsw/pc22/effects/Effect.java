428117c79206c19118b3261ab4a4b772b42c4ce1
Seve1995 <giuseppe.severino@mail.polimi.it>
2017-06-05 10:23:33 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/it/polimi/ingsw/pc22/effects/Effect.java
+++ src/main/java/it/polimi/ingsw/pc22/effects/Effect.java
@@ -1,10 +1,10 @@
-package it.polimi.ingsw.pc22.effects;
-
-import it.polimi.ingsw.pc22.gamebox.GameBoard;
-import it.polimi.ingsw.pc22.player.Player;
-
-public abstract class Effect
-{
-	protected abstract boolean isLegal(Player player, GameBoard gameBoard); 
-	public abstract void executeEffect(Player player, GameBoard gameBoard);
-}
+package it.polimi.ingsw.pc22.effects;
+
+import it.polimi.ingsw.pc22.gamebox.GameBoard;
+import it.polimi.ingsw.pc22.player.Player;
+
+public interface Effect
+{
+	boolean isLegal(Player player, GameBoard gameBoard); 
+	void executeEffect(Player player, GameBoard gameBoard);
+}
