17d064a5b8fb3a5d493d77911a356728d6112851
Seve1995 <giuseppe.severino@mail.polimi.it>
2017-06-08 12:45:01 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/it/polimi/ingsw/pc22/actions/SettingFamiliarMemberOnProduction.java
+++ src/main/java/it/polimi/ingsw/pc22/actions/SettingFamiliarMemberOnProduction.java
@@ -1,94 +1,94 @@
-package it.polimi.ingsw.pc22.actions;
-
-import it.polimi.ingsw.pc22.effects.DoProductionAction;
-import it.polimi.ingsw.pc22.gamebox.*;
-import it.polimi.ingsw.pc22.player.Player;
-
-public class SettingFamiliarMemberOnProduction extends Action {
-
-	public SettingFamiliarMemberOnProduction(FamilyMember familyMember)
-	{
-		super(familyMember);
-	}
-
-    public SettingFamiliarMemberOnProduction() {
-
-    }
-
-    @Override
-	protected boolean isLegal(Player player, GameBoard gameBoard) {
-		
-		Production production = gameBoard.getProduction();
-		
-		if (familyMember.getFamiliarValue() < 1)
-			return false;
-		
-		for (ProductionCell productionCell : production.getProductionCell()){
-			
-			FamilyMember currFamilyMember = productionCell.getFamilyMember();
-			
-			if (currFamilyMember == null) continue;
-			
-			if
-			(
-				currFamilyMember.getColor()==ColorsEnum.NEUTER ||
-						familyMember.getColor() == ColorsEnum.NEUTER
-			)
-				break;
-			
-			if (player.getFamilyMembers().contains(currFamilyMember))
-				return false;
-		}
-		
-		return true;
-		
-	}
-
-	@Override
-	public boolean executeAction(Player player, GameBoard gameBoard) {
-		
-		Production production = gameBoard.getProduction();
-		
-		DoProductionAction doProductionAction = new DoProductionAction();
-		
-		if (isLegal(player, gameBoard) && !(player.isDontCareOccupiedPlaces()))
-		{
-			
-			
-			production.getProductionCell()[production.firstCellFree()].setFamilyMember(familyMember);
-			
-			familyMember.setPlayed(true);
-			
-			if (production.firstCellFree()>0)
-			{
-				familyMember.setFamiliarValue(familyMember.getFamiliarValue() - 3);
-			}
-			
-			doProductionAction.setValue(familyMember.getValue());
-			
-			doProductionAction.executeEffect(player, gameBoard);
-			
-			return true;
-			
-		}
-		
-		else if (isLegal(player, gameBoard) && player.isDontCareOccupiedPlaces())
-		{
-			production.getProductionCell()[0].setFamilyMember(familyMember);
-
-			familyMember.setPlayed(true);
-			
-			doProductionAction.setValue(familyMember.getValue());
-			
-			doProductionAction.executeEffect(player, gameBoard);
-			
-			return true;
-		}
-		
-		else
-		{
-			return false;
-		}
-	}
-		
+package it.polimi.ingsw.pc22.actions;
+
+import it.polimi.ingsw.pc22.effects.DoProductionAction;
+import it.polimi.ingsw.pc22.gamebox.*;
+import it.polimi.ingsw.pc22.player.Player;
+
+public class SettingFamiliarMemberOnProduction extends Action {
+
+	public SettingFamiliarMemberOnProduction(FamilyMember familyMember)
+	{
+		super(familyMember);
+	}
+
+    public SettingFamiliarMemberOnProduction() {
+
+    }
+
+    @Override
+	protected boolean isLegal(Player player, GameBoard gameBoard) {
+		
+		Production production = gameBoard.getProduction();
+		
+		if (familyMember.getFamiliarValue() < 1)
+			return false;
+		
+		for (ProductionCell productionCell : production.getProductionCell()){
+			
+			FamilyMember currFamilyMember = productionCell.getFamilyMember();
+			
+			if (currFamilyMember == null) continue;
+			
+			if
+			(
+				currFamilyMember.getColor()==ColorsEnum.NEUTER ||
+				familyMember.getColor() == ColorsEnum.NEUTER
+			)
+				break;
+			
+			if (familyMember.getColor().equals(currFamilyMember.getColor())) //TODO: L'ho sistemata, va bene?
+				return false;
+		}
+		
+		return true;
+		
+	}
+
+	@Override
+	public boolean executeAction(Player player, GameBoard gameBoard) {
+		
+		Production production = gameBoard.getProduction();
+		
+		DoProductionAction doProductionAction = new DoProductionAction();
+		
+		if (isLegal(player, gameBoard) && !(player.isDontCareOccupiedPlaces())) //Non dovrebbe essere OR?
+		{
+			
+			
+			production.getProductionCell()[production.firstCellFree()].setFamilyMember(familyMember);
+			
+			familyMember.setPlayed(true);
+			
+			if (production.firstCellFree()>0)
+			{
+				familyMember.setFamiliarValue(familyMember.getFamiliarValue() - 3);
+			}
+			
+			doProductionAction.setValue(familyMember.getValue());
+			
+			doProductionAction.executeEffect(player, gameBoard);
+			
+			return true;
+			
+		}
+		
+		else if (isLegal(player, gameBoard) && player.isDontCareOccupiedPlaces())
+		{
+			production.getProductionCell()[0].setFamilyMember(familyMember);
+
+			familyMember.setPlayed(true);
+			
+			doProductionAction.setValue(familyMember.getValue());
+			
+			doProductionAction.executeEffect(player, gameBoard);
+			
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}
+		
 }
